20d7792fef20055e1f99eac8cad165e4a8c4cf5b
Gabes <gabnology+github@gmail.com>
2022-09-05 17:04:04 +0000

    Add some code

--- public/index.js
+++ public/index.js
@@ -1,1 +1,19 @@
-console.log("Hiiii")
+let connected = false
+
+const statusspan = document.getElementById("status")
+
+const url = `${window.location.origin}/`
+
+function setStatus(status){
+    statusspan.innerHTML = status
+}
+
+function connect(){
+    let channel = prompt("Enter channel name:");
+    if(channel === null || channel === ""){
+        connect();
+    }
+    let username = prompt("Enter username:");
+    
+}
+connect()
